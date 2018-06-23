1aba0d86822827408eca41b9bf28cdd45fb90d3e
Matte <mattias4@kth.se>
2018-06-23 17:37:51 +0200

    More tests

--- python/list_index/p2.py
+++ python/list_index/p2.py
@@ -1,14 +1,14 @@
+import random
 import time
+from string import ascii_letters as alphabet
 
-WORD = u"lorem ipsum dolor sit amet " * 100
-
-for i in range(1, 10):
-    n = i * 500000
-
-    words = [u"{} - {}".format(WORD, i) for i in range(n)]
+for i in range(1, 11):
+    words = [''.join(random.sample(list(alphabet), len(alphabet))) for _ in range(i * 100000)]
+    word = words[0]
+    random.shuffle(words)
 
     begin = time.time()
-    words.index(words[-1])
+    words.index(word)
     elapsed = time.time() - begin
 
     print("{:f}\t".format(elapsed))
