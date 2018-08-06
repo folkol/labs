4dd2e7fb88acf0929de6970a0ddf446219a70fa9
Matte <mattias4@kth.se>
2018-08-06 20:36:04 +0200

    Entering raw mode

--- raw_term/main.c
+++ raw_term/main.c
@@ -1,11 +1,20 @@
+#include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <termios.h>
 
 
+struct termios original;
+
+void reset_termios() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original);
+}
+
 int main() {
     struct termios raw;
-    tcgetattr(STDIN_FILENO, &raw);
+    tcgetattr(STDIN_FILENO, &original);
+    atexit(reset_termios);
+    raw = original;
     raw.c_lflag &= ~(ECHO);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 
