1803d5930c178449dc06fe2b8449a5fe08db7c72
Matte <folkol@mailbox.org>
2026-01-01 17:01:22 +0100

    always usafe, yolo

--- 1brc/onebrc/src/bin/thomaswue_ported.rs
+++ 1brc/onebrc/src/bin/thomaswue_ported.rs
@@ -154,7 +154,7 @@ impl Result {
 fn parse_loop(
     data: &[u8],
     _cursor: &AtomicUsize,
-    _file_end: usize,
+    file_end: usize,
     _file_start: usize,
 ) -> Vec<Result> {
     let mut table: Vec<u32> = vec![0; HASH_TABLE_SIZE as usize];
@@ -164,7 +164,7 @@ fn parse_loop(
 
     loop {
         let current = _cursor.fetch_add(SEGMENT_SIZE as usize, Ordering::Relaxed);
-        if current >= _file_end {
+        if current >= file_end {
             let mut results = Vec::with_capacity(station_keys.len());
             for (key, stat) in station_keys.iter().zip(station_stats) {
                 results.push(Result {
@@ -180,28 +180,25 @@ fn parse_loop(
             return results;
         }
 
-        let segment_end = next_newline(
-            data,
-            usize::min(_file_end - 1, current + SEGMENT_SIZE as usize),
-        );
-
         let segment_start = if current == _file_start {
             current
         } else {
             next_newline(data, current) + 1
         };
 
-        if current == 0 {
-            assert_eq!(segment_start, 0);
-        }
+        let segment_end_nl = next_newline(
+            data,
+            usize::min(file_end - 1, current + SEGMENT_SIZE as usize),
+        );
+        let segment_end = segment_end_nl + 1;
 
         let dist = (segment_end - segment_start) / 3;
-        let midpoint_1 = next_newline(data, segment_start + dist);
-        let midpoint_2 = next_newline(data, segment_start + dist + dist);
+        let mid1_nl = next_newline(data, segment_start + dist);
+        let mid2_nl = next_newline(data, segment_start + 2 * dist);
 
-        let mut scanner_1 = Scanner::new(data, segment_start, midpoint_1, _file_end);
-        let mut scanner_2 = Scanner::new(data, midpoint_1 + 1, midpoint_2, _file_end);
-        let mut scanner_3 = Scanner::new(data, midpoint_2 + 1, segment_end, _file_end);
+        let mut scanner_1 = Scanner::new(data, segment_start, mid1_nl + 1);
+        let mut scanner_2 = Scanner::new(data, mid1_nl + 1, mid2_nl + 1);
+        let mut scanner_3 = Scanner::new(data, mid2_nl + 1, segment_end);
 
         while scanner_1.has_next_safe() && scanner_2.has_next_safe() && scanner_3.has_next_safe() {
             let word_1 = scanner_1.get_u64_unsafe();
@@ -229,7 +226,7 @@ fn parse_loop(
                 &mut table,
                 &mut station_keys,
                 &mut station_stats,
-                _file_end,
+                file_end,
             );
             let index2 = find_result_unsafe_idx(
                 word_2,
@@ -240,7 +237,7 @@ fn parse_loop(
                 &mut table,
                 &mut station_keys,
                 &mut station_stats,
-                _file_end,
+                file_end,
             );
             let index3 = find_result_unsafe_idx(
                 word_3,
@@ -251,7 +248,7 @@ fn parse_loop(
                 &mut table,
                 &mut station_keys,
                 &mut station_stats,
-                _file_end,
+                file_end,
             );
 
             let number_1 = scan_number_unsafe(&mut scanner_1);
@@ -263,13 +260,13 @@ fn parse_loop(
             unsafe { stat_get_mut(&mut station_stats, index3).record(number_3) };
         }
 
-        while scanner_1.has_next() {
-            let word_1 = scanner_1.get_u64();
+        while scanner_1.has_next_safe() {
+            let word_1 = scanner_1.get_u64_unsafe();
             let delim_1 = find_delimiter(word_1);
-            let word_1b = scanner_1.get_u64_at(scanner_1.pos + 8);
+            let word_1b = scanner_1.get_u64_at_unsafe(scanner_1.pos + 8);
             let delim_1b = find_delimiter(word_1b);
 
-            let index = find_result_idx(
+            let index = find_result_unsafe_idx(
                 word_1,
                 delim_1,
                 word_1b,
@@ -278,19 +275,19 @@ fn parse_loop(
                 &mut table,
                 &mut station_keys,
                 &mut station_stats,
-                _file_end,
+                file_end,
             );
 
-            let number_1 = scan_number(&mut scanner_1); // <-- THIS ONE PANICS
+            let number_1 = scan_number_unsafe(&mut scanner_1);
             unsafe { stat_get_mut(&mut station_stats, index).record(number_1) };
         }
-        while scanner_2.has_next() {
-            let word_2 = scanner_2.get_u64();
+        while scanner_2.has_next_safe() {
+            let word_2 = scanner_2.get_u64_unsafe();
             let delim_2 = find_delimiter(word_2);
-            let word_2b = scanner_2.get_u64_at(scanner_2.pos + 8);
+            let word_2b = scanner_2.get_u64_at_unsafe(scanner_2.pos + 8);
             let delim_2b = find_delimiter(word_2b);
 
-            let index = find_result_idx(
+            let index = find_result_unsafe_idx(
                 word_2,
                 delim_2,
                 word_2b,
@@ -299,19 +296,19 @@ fn parse_loop(
                 &mut table,
                 &mut station_keys,
                 &mut station_stats,
-                _file_end,
+                file_end,
             );
 
-            let number_2 = scan_number(&mut scanner_2);
+            let number_2 = scan_number_unsafe(&mut scanner_2);
             unsafe { stat_get_mut(&mut station_stats, index).record(number_2) };
         }
-        while scanner_3.has_next() {
-            let word_3 = scanner_3.get_u64();
+        while scanner_3.has_next_safe() {
+            let word_3 = scanner_3.get_u64_unsafe();
             let delim_3 = find_delimiter(word_3);
-            let word_1b = scanner_3.get_u64_at(scanner_3.pos + 8);
+            let word_1b = scanner_3.get_u64_at_unsafe(scanner_3.pos + 8);
             let delim_1b = find_delimiter(word_1b);
 
-            let index = find_result_idx(
+            let index = find_result_unsafe_idx(
                 word_3,
                 delim_3,
                 word_1b,
@@ -320,24 +317,13 @@ fn parse_loop(
                 &mut table,
                 &mut station_keys,
                 &mut station_stats,
-                _file_end,
+                file_end,
             );
 
-            let number_3 = scan_number(&mut scanner_3);
+            let number_3 = scan_number_unsafe(&mut scanner_3);
             unsafe { stat_get_mut(&mut station_stats, index).record(number_3) };
         }
     }
-}
-
-#[inline(always)]
-fn scan_number(scanner: &mut Scanner) -> i16 {
-    let number_word: u64 = scanner.get_u64_at(scanner.pos() + 1);
-    let decimal_sep_pos: u32 = ((!number_word) & 0x1010_1000u64).trailing_zeros();
-    let number: i16 = convert_into_number_i16(decimal_sep_pos, number_word);
-    let adv: usize = ((decimal_sep_pos >> 3) as usize) + 4;
-    scanner.add(adv);
-
-    number
 }
 
 /// Port of the Java `scanNumber`, returning `i16`.
@@ -378,7 +364,7 @@ struct Scanner<'a> {
 }
 
 impl<'a> Scanner<'a> {
-    fn new(data: &'a [u8], start: usize, end: usize, _file_end: usize) -> Scanner<'a> {
+    fn new(data: &'a [u8], start: usize, end: usize) -> Scanner<'a> {
         Self {
             data,
             end,
@@ -386,45 +372,24 @@ impl<'a> Scanner<'a> {
         }
     }
 
-    fn has_next(&self) -> bool {
+    #[inline(always)]
+    fn has_next_safe(&self) -> bool {
         self.pos < self.end
     }
 
-    fn has_next_safe(&self) -> bool {
-        self.pos + 16 < self.end
-    }
-
-    #[inline]
+    #[inline(always)]
     pub fn pos(&self) -> usize {
         self.pos
     }
 
-    #[inline]
+    #[inline(always)]
     pub fn add(&mut self, n: usize) {
         self.pos += n
     }
 
-    #[inline]
-    pub fn get_u64(&mut self) -> u64 {
-        self.get_u64_at(self.pos)
-    }
-
-    #[inline]
+    #[inline(always)]
     pub fn get_u64_unsafe(&mut self) -> u64 {
         self.get_u64_at_unsafe(self.pos)
-    }
-
-    #[inline]
-    pub fn get_u64_at(&self, off: usize) -> u64 {
-        let mut buf = [0u8; 8];
-
-        let len = self.data.len();
-        if off < len {
-            let n = (len - off).min(8);
-            buf[..n].copy_from_slice(&self.data[off..off + n]);
-        }
-
-        u64::from_le_bytes(buf)
     }
 
     #[inline(always)]
@@ -440,45 +405,8 @@ fn find_delimiter(word: u64) -> u64 {
 }
 
 #[inline]
-pub fn next_newline_safe(bytes: &[u8], mut prev: usize) -> usize {
-    loop {
-        let word = u64::from_le_bytes(bytes[prev..prev + 8].try_into().unwrap());
-
-        let input = word ^ 0x0A0A0A0A0A0A0A0Au64;
-        let pos = (input.wrapping_sub(0x0101010101010101u64)) & !input & 0x8080808080808080u64;
-
-        if pos != 0 {
-            prev += (pos.trailing_zeros() as usize) >> 3;
-            return prev;
-        } else {
-            prev += 8;
-        }
-    }
-}
-
-#[inline]
 pub fn next_newline(data: &[u8], prev: usize) -> usize {
     prev + memchr::memchr(b'\n', &data[prev..]).expect("expected newline")
-}
-
-#[inline]
-pub unsafe fn next_newline_ptr(mut p: *const u8) -> *const u8 {
-    unsafe {
-        loop {
-            let word = (p as *const u64).read_unaligned(); // native endian load
-            let word = u64::from_le(word); // match little-endian files / x86 behavior
-
-            let input = word ^ 0x0A0A0A0A0A0A0A0Au64;
-            let pos = (input.wrapping_sub(0x0101010101010101u64)) & !input & 0x8080808080808080u64;
-
-            if pos != 0 {
-                p = p.add((pos.trailing_zeros() as usize) >> 3);
-                return p;
-            } else {
-                p = p.add(8);
-            }
-        }
-    }
 }
 
 #[repr(C)]
@@ -519,32 +447,6 @@ fn hash_to_index(hash: u64, table_len: usize) -> usize {
     debug_assert!(table_len.is_power_of_two());
     let hash_as_int = hash ^ (hash >> 33) ^ (hash >> 15);
     (hash_as_int as usize) & (table_len - 1)
-}
-
-fn new_entry(
-    name_address: usize,
-    name_length: usize,
-    scanner: &Scanner,
-) -> (StationKey, StationStats) {
-    let mut key = StationKey {
-        first: scanner.get_u64_at(name_address),
-        second: scanner.get_u64_at(name_address + 8),
-        name_address,
-    };
-    let total_length = name_length + 1;
-    if total_length <= 8 {
-        key.first &= MASK1[total_length - 1];
-        key.second = 0;
-    } else if total_length < 16 {
-        key.second &= MASK1[total_length - 9];
-    }
-    let stat = StationStats {
-        min: i16::MAX,
-        max: i16::MIN,
-        sum: 0,
-        count: 0,
-    };
-    (key, stat)
 }
 
 fn new_entry_unsafe(
@@ -598,120 +500,6 @@ const MASK2: [u64; 9] = [
 ];
 
 #[inline(always)]
-fn find_result_idx<'a, 'b>(
-    initial_word: u64,
-    initial_delim_mask: u64,
-    word_b: u64,
-    delim_mask_b: u64,
-    scanner: &mut Scanner<'a>,
-    table: &mut [u32],
-    station_keys: &mut Vec<StationKey>,
-    station_stats: &mut Vec<StationStats>,
-    file_end: usize,
-) -> usize {
-    let mut word = initial_word;
-    let mut delimiter_mask = initial_delim_mask;
-    let mut hash: u64;
-    let name_address = scanner.pos;
-    let mut word2 = word_b;
-    let delimiter_mask2 = delim_mask_b;
-
-    if scanner.pos > file_end - 120 {
-        hash = word ^ word2;
-        while scanner.pos < scanner.end {
-            let b = scanner.data[scanner.pos];
-            if b == b';' {
-                break;
-            }
-            hash ^= b as u64;
-            scanner.add(1);
-        }
-    } else if (delimiter_mask | delimiter_mask2) != 0 {
-        let letter_count1 = (delimiter_mask.trailing_zeros() as usize) >> 3;
-        let letter_count2 = (delimiter_mask2.trailing_zeros() as usize) >> 3;
-
-        let mask = MASK2[letter_count1];
-        word &= MASK1[letter_count1];
-        word2 = mask & word2 & MASK1[letter_count2];
-
-        hash = word ^ word2;
-
-        let i1 = letter_count1 + ((letter_count2 as u64 & mask) as usize);
-        scanner.add(i1);
-
-        let idx = hash_to_index(hash, table.len());
-
-        let entry = table[idx];
-        if entry != 0 {
-            let r = &station_keys[entry as usize - 1];
-            if r.first == word && r.second == word2 {
-                return (entry - 1) as usize;
-            }
-        }
-    } else {
-        hash = word ^ word2;
-        scanner.add(16);
-
-        loop {
-            word = scanner.get_u64();
-            delimiter_mask = find_delimiter(word);
-
-            if delimiter_mask != 0 {
-                let tz = delimiter_mask.trailing_zeros() as usize;
-                word <<= 63 - tz;
-                scanner.add(tz >> 3);
-                hash ^= word;
-                break;
-            } else {
-                scanner.add(8);
-                hash ^= word;
-            }
-        }
-    }
-
-    let name_length = scanner.pos() - name_address;
-
-    let mut table_index = hash_to_index(hash, table.len());
-    let mask = table.len() - 1;
-
-    'outer: loop {
-        let mut entry = table[table_index];
-        if entry == 0 {
-            let (key, stat) = new_entry(name_address, name_length, scanner);
-            station_keys.push(key);
-            station_stats.push(stat);
-            entry = station_keys.len() as u32;
-            unsafe { table_set(table, table_index, entry) };
-            return entry as usize - 1;
-        }
-
-        let idx = (table[table_index] - 1) as usize;
-        let existing_addr = unsafe { key_get(station_keys, idx).name_address };
-
-        let end = name_length + 1;
-        let mut i: usize = 0;
-        while i + 8 < end {
-            if scanner.get_u64_at(existing_addr + i) != scanner.get_u64_at(name_address + i) {
-                table_index = (table_index + 31) & mask;
-                continue 'outer;
-            }
-            i += 8;
-        }
-
-        let remaining_shift = 64 - ((end - i) << 3);
-
-        let a = scanner.get_u64_at(existing_addr + i);
-        let b = scanner.get_u64_at(name_address + i);
-
-        if ((a ^ b) << remaining_shift) == 0 {
-            return idx;
-        } else {
-            table_index = (table_index + 31) & mask;
-        }
-    }
-}
-
-#[inline(always)]
 unsafe fn table_get(table: &[u32], i: usize) -> u32 {
     unsafe { *table.get_unchecked(i) }
 }
@@ -730,7 +518,7 @@ unsafe fn stat_get_mut(out: &mut [StationStats], i: usize) -> &mut StationStats 
     unsafe { out.get_unchecked_mut(i) }
 }
 
-#[inline(never)]
+#[inline(always)]
 fn find_result_unsafe_idx<'a, 'b>(
     initial_word: u64,
     initial_delim_mask: u64,
